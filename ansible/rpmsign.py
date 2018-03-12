d66a61d33c9938325e48406e1cf15c58d7913b1e
Johnathon Hall <john.hall7688@hotmail.com>
2018-03-12 11:30:05 -0600

    Update rpmsign.py
    Fixed typo in fail message

--- ansible/rpmsign.py
+++ ansible/rpmsign.py
@@ -38,7 +38,6 @@ options:
       - Present adds a signature, absent will remove a signature
     required: false
     default: "present"
-
 # informational: requirements for nodes
 requirements:
     - rpm 
@@ -59,7 +58,7 @@ EXAMPLES = '''
     marcos:
       _signature: gpg
       _gpg_name: gpg key name
-      
+
 # Remove signature from packages
 - rpmsign:
     directory:
@@ -73,19 +72,88 @@ import os
 
 try:
     import rpm
+
     HAS_RPM = True
 except ImportError:
     HAS_RPM = False
 
 from ansible.module_utiles.basic import AnsibleModule
 
+def sign(module):
+    """Add signature to packages"""
+    results = {
+        "changed": False,
+        "results": [],
+        "changes": []
+    }
+    if not module.params['passphrase'] and not module.params['key']:
+        module.fail_json(rc=1, msg='Error: Both passphrase and key are '
+                                   'required when signing an rpm')
+    else:
+        if module.params['macros']:
+            for macro, value in module.params['macros'].items():
+                rpm.addMacro(macro, value)
+        for package in module.params['packages']:
+            pyread, cwrite = os.pipe()
+            write = os.fdopen(cwrite, 'w')
+            rpm.setLogFile(cwrite)
+            result = rpm.addSign(
+                '{rpm}'.format(rpm=package),
+                module.params['passphrase'], module.params['key']
+            )
+            write.close()
+            pyread = os.fdopen(pyread)
+            msg = pyread.readline()
+            pyread.close()
+
+            if not result:
+                module.fail_json(rc=1, msg='Error: Failed to sign {rpm}'.format(rpm=package))
+
+            if not msg:
+                results['changes'].append('{}'.format(package))
+                results['results'].append('{} was signed'.format(package))
+                if not results['changed']:
+                    results['changed'] = True
+            else:
+                results['results'].append('{} skipped, already signed'.format(package))
+        module.exit_json(
+            changed=results['changed'],
+            results=results['results'],
+            changes=dict(signed=results['changes'])
+        )
+
+def del_sign(module):
+    """remove signature from packages"""
+    results = {
+        "changed": False,
+        "results": [],
+        "changes": []
+    }
+    for package in module.params['rpms']:
+        rpm.delSign('{rpm}'.format(rpm=package))
+        results['changes'].append('{}'.format(package))
+        results['results'].append('removed signature from {}'.format(package))
+        if not results['changed']:
+            results['changed'] = True
+    module.exit_json(
+        changed=results['changed'],
+        results=results['results'],
+        changes=dict(removed=results['changes'])
+    )
+
 def main():
+    """ This ansible module uses the c bindings around rpm
+    to either sign an rpm or remove a signature.
+    """
     module = AnsibleModule(
         argument_spec=dict(
-            passphrase=dict(type='str', required=False, default=None, no_log=True),
-            key=dict(type='str', required=False, default=None),
+            passphrase=dict(type='str', required=False,
+                            default=None, no_log=True),
+            key=dict(type='str', required=False,
+                     default=None),
             rpms=dict(type='list', required=True),
-            state=dict(type='str', required=False, default='present', choices=['present', 'absent']),
+            state=dict(type='str', required=False,
+                       default='present', choices=['present', 'absent']),
             macros=dict(type='dict', required=False, default=None)
         ),
         supports_check_mode=True
@@ -94,59 +162,11 @@ def main():
     if not HAS_RPM:
         module.fail_json(rc=1, msg='Error: python2 rpm module is needed for this ansible module')
 
-    results = {
-        "changed": False,
-        "results": [],
-        "changes": []
-    }
-
     if module.params['state'] == "present":
-        if not module.params['passphrase'] and not module.params['key']:
-            module.fail_json(rc=1, msg='Error: Both passphrase and key are required when signing an rpm')
-        else:
-            if module.params['macros']:
-                for macro, value in module.params['macros'].items():
-                    rpm.addMacro(macro, value)
-            for package in module.params['packages']:
-                r, w = os.pipe()
-                w = os.fdopen(w, 'w')
-                rpm.setLogFile(w)
-                result = rpm.addSign(
-                    '{rpm}'.format(rpm=package),
-                    module.params['passphrase'], module.params['key']
-                )
-                w.close()
-                r = os.fdopen(r)
-                msg = r.readline()
-                r.close()
-                
-                if not result:
-                    module.fail_json(rc=1, msg='Error: Failed to sign {rpm}'.format(package))
-                
-                if not msg:
-                    results['changes'].append('{}'.format(package))
-                    results['results'].append('{} was signed'.format(package))
-                    if not results['changed']:
-                        results['changed'] = True
-                else:
-                    results['results'].append('{} skipped, already signed'.format(package))
-            module.exit_json(
-                changed=results['changed'],
-                results=results['results'],
-                changes=dict(signed=results['changes'])
-            )
+        sign(module)
     else:
-        for package in module.params['rpms']:
-            rpm.delSign('{rpm}'.format(rpm=package))
-            results['changes'].append('{}'.format(package))
-            results['results'].append('removed signature from {}'.format(package))
-            if not results['changed']:
-                results['changed'] = True
-        module.exit_json(
-            changed=results['changed'],
-            results=results['results'],
-            changes=dict(removed=results['changes'])
-        )
+        del_sign(module)
+
 
 if __name__ == "__main__":
     main()
